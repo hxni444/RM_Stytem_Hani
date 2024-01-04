b6c4f4a53a9d924c1ffcbd0ccd37682fd293188c
hxni444 <107667722+hxni444@users.noreply.github.com>
2024-01-04 17:40:56 +0530

    linq

---
+++ ASP.net/HandsOnLinq/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace HandsOnLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = {0, 45, 125, 458, 8, 54, 12, 68, 65, 65, };
+            var rslt= from x in n
+                      where x>30
+                      select x;
+
+
+            rslt = from x in n
+                   select x * x;
+
+
+            rslt = from x in n
+                   where x%2==0
+                   select x;
+
+            rslt= from x in n
+                  where x>100&& x>45
+                  select x;
+
+            rslt = from x in n
+                   orderby x
+                   select x;
+
+
+            foreach( var x in rslt )
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
---
+++ ASP.net/HandsOnLinq/strings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOnLinq
+{
+    internal class strings
+    {
+        public static void Main()
+        {
+            string name = "hani al ziya";
+             string[] fruit = { "banana", "tomato", "kiwi", "apple" };
+        string[] flowers = { "rose", "jasmine", "sun flower", "dahliya", "hibiscus" };
+
+        var rslt = from f in fruit
+                   where f.Length> 5
+                   select f;
+
+          /*  foreach ( var f in rslt )
+            {
+                Console.WriteLine( f );
+            }
+*/
+            var rslt2 = from f in flowers
+                        orderby f
+                        select f;
+
+            var ch= from k in name
+                    orderby k
+                    select k;
+
+            foreach( var f in ch)
+            {
+                Console.WriteLine( f );
+            }
+        }
+       
+    }
+}
