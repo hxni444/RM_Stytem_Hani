a87e58860b374df5da81b5897174a8daaff8d0d5
hxni444 <107667722+hxni444@users.noreply.github.com>
2024-01-08 13:44:08 +0530

    Assignment_9

---
+++ Assignment_9(validation)/Assignment_9(validation)/Model/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace Assignment_9_validation_.Model
+{
+    public class Movie
+    {
+        public int MovieId;
+        public string MovieName, Lang, Actor, Director;
+        public DateTime ReleaseYear; 
+    }
+}
---
+++ Assignment_9(validation)/Assignment_9(validation)/Repository/MovieRepo.cs
@@ -0,0 +1,49 @@
+﻿using Assignment_9_validation_.Model;
+
+namespace Assignment_9_validation_.Repository
+{
+    public class MovieRepo : ImovieRepo
+    {
+        List<Movie> movie_List = new List<Movie>();
+        public void AddMovie()
+        {
+            movie_List.Add(new Movie());
+            
+        }
+
+        public List<Movie> GetAllMovies()
+        {
+            return movie_List;
+        }
+
+        public Movie GetMovieByName(string MovName)
+        {
+            //List <Movie> MovNameList = new List<Movie> ();
+            foreach (var k in movie_List)
+            {
+                if(k.MovieName == MovName)
+                {
+                    return k;
+                }
+               
+            }
+            return null;
+           
+        }
+
+        public Movie GetMoviesByActor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Movie GetMoviesByDirector()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Movie GetMoviesByReleaseYear()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
